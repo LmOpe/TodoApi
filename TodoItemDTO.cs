97063e282859b7ded1fc7af2a6ea2c7436fb6958
Muhammed <lawalmuhammedope@gmail.com>
2025-10-05 03:02:49 +0100

    Add Secret column to Todo model and update migrations

---
+++ TodoItemDTO.cs
@@ -0,0 +1,12 @@
+namespace TodoApi;
+
+public class TodoItemDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItemDTO() { }
+    public TodoItemDTO(Todo todoitem) =>
+    (Id, Name, IsComplete) = (todoitem.Id, todoitem.Name, todoitem.IsComplete);
+}
